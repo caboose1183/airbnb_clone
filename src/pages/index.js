0224b733b9a03412219c502107d5af09a8cf1418
Sidney Pang <sidneypang@hotmail.co.uk>
2023-05-12 19:35:54 -0400

    header component finished

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,20 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import Head from "next/head";
+import { Inter } from "next/font/google";
+import Header from "@/components/Header";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main
+    <main className="">
+      <Head>
+        <title>Airbnb Clone</title>
+      </Head>
       
-    >
-      
+      <Header />
+
+      {/* Banner */}
     </main>
-  )
+  );
 }
